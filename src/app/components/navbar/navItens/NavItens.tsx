3030d037a6782efcf00fcd50bc97f870670d7e28
CriticalNoob02 <adrieldchavea20@gmail.com>
2023-08-11 15:57:07 -0300

    feat: add optional link param

--- src/app/components/navbar/navItens/NavItens.tsx
+++ src/app/components/navbar/navItens/NavItens.tsx
@@ -5,7 +5,7 @@ export default function NavItens({ data }: NavItensProps) {
         <div className="w-full h-full flex items-center justify-evenly text-2xl text-red-950">
             {data.titles.map((title, i) => (
                 <h1 key={data.uuid[i]} className="hover:scale-150 transition ease-in-out delay-150">
-                    <a href={data.links[i]}>
+                    <a href={data.links ?data.links[i]: '#'}>
                         {title}
                     </a>
                 </h1>
