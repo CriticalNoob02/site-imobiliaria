eb372df820a5971e1fd5a7c929ca96f18cc2e75f
CriticalNoob02 <adrieldchavea20@gmail.com>
2023-08-11 10:41:04 -0300

    refactor: generalizing component

--- src/app/components/navbar/Navbar.tsx
+++ src/app/components/navbar/Navbar.tsx
@@ -1,9 +1,25 @@
 import { randomUUID } from "crypto"
 import Image from "../../../../node_modules/next/image"
 import logo from "../../../../public/pic.jpg"
-import NavItens from "./navItens/NavItens"
+import MenuToogle from "./menuToogle/MenuToogle"
 
 export default function Navbar () {
+    const title = [
+        'AAA',
+        'BBB',
+        'CCC'
+    ]
+    const link = [
+        '#AAA',
+        '#BBB',
+        '#CCC'
+    ]
+    const uuids = [
+        randomUUID(),
+        randomUUID(),
+        randomUUID()
+    ]
+
     return(
         <div className="w-screen h-28 flex justify-around fixed p-2 drop-shadow-xl backdrop-blur-sm bg-red-200/60">
             <div className="basis-1/3 overflow-hidden">
@@ -15,11 +31,7 @@ export default function Navbar () {
             </div>
             <div className="basis-2/3">
                 <div className="w-full h-full">
-                  <NavItens data={{
-                        titles: ["Ab","aee","eef"],
-                        links: ["#12","#15","#20"],
-                        uuid: [randomUUID(), randomUUID(), randomUUID()]
-                    }}/>
+                    <MenuToogle title={title} link={link} uuids={uuids}/>
                 </div>
             </div>
         </div>
--- src/app/components/navbar/navItens/types/index.ts
+++ src/app/components/navbar/navItens/types/index.ts
@@ -2,11 +2,13 @@
  * @param titles Titulo do item;
  * @param links Link de referencia;
  * @param uuid Key, usar a função "randomUUID";
+ * @param status
  */
 export default interface NavItensProps {
     data:{
         titles:Array<string>
-        links:Array<string>
+        links?:Array<string>
         uuid:Array<string>
     }
+    status?: boolean
 }
