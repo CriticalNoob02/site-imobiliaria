2d422243154dbb1498ebad300a92ca84113bd76b
CriticalNoob02 <adrieldchavea20@gmail.com>
2023-08-11 17:41:53 -0300

    feat: styleze links to menu

--- src/app/components/navbar/navItens/NavItens.tsx
+++ src/app/components/navbar/navItens/NavItens.tsx
@@ -2,13 +2,15 @@ import NavItensProps from "./types/index"
 
 export default function NavItens({ data }: NavItensProps) { 
     return ( 
-        <div className="w-full h-full flex items-center justify-evenly text-2xl text-red-950">
+        <div className="w-full h-full flex flex-col mt-14">
             {data.titles.map((title, i) => (
-                <h1 key={data.uuid[i]} className="hover:scale-150 transition ease-in-out delay-150">
-                    <a href={data.links ?data.links[i]: '#'}>
-                        {title}
-                    </a>
-                </h1>
+                <div key={data.uuid[i]} className="drop-shadow-xl backdrop-blur-sm bg-red-800/60 mb-10 py-3 px-5 hover:scale-110 transition ease-in-out delay-150">
+                    <h1 className="font-semibold text-slate-100">
+                        <a href={data.links ?data.links[i]: '#'}>
+                            {title}
+                        </a>
+                    </h1>
+                </div>
             ))}
         </div>
     );
