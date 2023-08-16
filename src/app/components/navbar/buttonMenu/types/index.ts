3538c9168d48783e5a5c35d748a63892951e1b2a
CriticalNoob02 <adrieldchavea20@gmail.com>
2023-08-16 17:07:32 -0300

    refactor: change componente ButtonMenu

---
+++ src/app/components/navbar/buttonMenu/types/index.ts
@@ -0,0 +1,4 @@
+export default interface ButtonMenuProps {
+    status:boolean
+    click:React.ReactNode
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,9 @@
-import Image from "../../node_modules/next/image"
-import pic from "../../public/pic.jpg"
 import CarouselBanner from "./components/carouselBanner/CarouselBanner"
 
 export default function Home() {
   return (
     <main>
       <CarouselBanner/>
-      <div><h1>AAAAA</h1></div>
     </main>
   )
 }
