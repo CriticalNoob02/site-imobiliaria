ac7597a3cdb263c383968abd25526dcf55ea99ca
CriticalNoob02 <adrieldchavea20@gmail.com>
2023-08-16 11:26:45 -0300

    feat: create glass banner

--- src/app/components/carouselBanner/CarouselBanner.tsx
+++ src/app/components/carouselBanner/CarouselBanner.tsx
@@ -1,18 +1,9 @@
-import Image from "../../../../node_modules/next/image"
-import pic from "../../../../public/pic.jpg"
-
 export default function  CarouselBanner () {
     return(
-        <div className="w-screen h-screen">
-            <Image
-            src={pic}
-            alt=""
-            fill/>
-            <div className="Vidro backdrop-blur-sm bg-white-200/20 w-full h-full">
-                <div className="card">
-                    <div className="Texto">
-                        <input className="botões"></input>
-                        <div className="Gradiente"></div>
+        <div className="w-screen h-screen card">
+            <div className="backdrop-blur-sm bg-white-200/20 w-full h-full flex items-center justify-center">
+                <div className="overflow-hidden w-3/5 h-2/5 flex items-center justify-center shadow-2xl rounded outline outline-offset-1 outline-neutral-500/10">
+                    <div className="w-screen h-screen card">
                     </div>
                 </div>
             </div>
