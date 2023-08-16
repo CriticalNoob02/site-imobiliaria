dceb810cb804a5783fb38e6b6ea5a5bba9fe771c
CriticalNoob02 <adrieldchavea20@gmail.com>
2023-08-16 12:34:38 -0300

    feat: add icon in buttonContact

--- src/app/components/navbar/buttonContact/ButtonContact.tsx
+++ src/app/components/navbar/buttonContact/ButtonContact.tsx
@@ -1,4 +1,5 @@
 import ButtonContactProps from "./types";
+import { BsWhatsapp } from "react-icons/bs";
 
 export default function ButtonContact ({
     link,
@@ -6,8 +7,9 @@ export default function ButtonContact ({
 }:ButtonContactProps) {
     return(
         <div>
-             <button className="p-3 text-lg font-semibold transition text-slate-100 rounded bg-lime-600 hover:bg-lime-500 hover:shadow-lg hover:shadow-lime-600/50 hover:-translate-y-0.5 ">
-                <a href={link} target="_blank">
+             <button className="p-3 text-lg font-semibold transition text-slate-100 rounded bg-lime-600 hover:bg-lime-500 hover:shadow-lg hover:shadow-lime-600/50 hover:-translate-y-0.5 flex items-center justify-center">
+                <BsWhatsapp/>
+                <a href={link} target="_blank" className="mx-1">
                     {title}
                 </a>
             </button>
