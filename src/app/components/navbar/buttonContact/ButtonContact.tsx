bff0c313b8707b7813058245d368a8c823b5c53d
CriticalNoob02 <adrieldchavea20@gmail.com>
2023-08-16 15:08:45 -0300

    refactor: change props in button

--- src/app/components/navbar/buttonContact/ButtonContact.tsx
+++ src/app/components/navbar/buttonContact/ButtonContact.tsx
@@ -1,14 +1,14 @@
 import ButtonContactProps from "./types";
-import { BsWhatsapp } from "react-icons/bs";
 
 export default function ButtonContact ({
     link,
-    title
+    title,
+    icon
 }:ButtonContactProps) {
     return(
         <div>
              <button className="p-3 text-lg font-semibold transition text-slate-100 rounded bg-lime-600 hover:bg-lime-500 hover:shadow-lg hover:shadow-lime-600/50 hover:-translate-y-0.5 flex items-center justify-center">
-                <BsWhatsapp/>
+                {icon}
                 <a href={link} target="_blank" className="mx-1">
                     {title}
                 </a>
--- src/app/components/navbar/buttonContact/types/index.ts
+++ src/app/components/navbar/buttonContact/types/index.ts
@@ -1,8 +1,10 @@
 /**
  * @param link link de contato;
  * @param title Texto exibido no botão;
+ * @param icon Icone para o botão
  */
 export default interface ButtonContactProps {
     link:string
     title:string
+    icon?:React.ReactNode
 }
