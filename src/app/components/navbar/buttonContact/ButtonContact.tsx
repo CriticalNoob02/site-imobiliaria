951c63c1c3f1cf08dd5b7dccb5c912cba94368aa
CriticalNoob02 <adrieldchavea20@gmail.com>
2023-08-17 17:28:30 -0300

    feat: add use select none in components

--- src/app/components/navbar/buttonContact/ButtonContact.tsx
+++ src/app/components/navbar/buttonContact/ButtonContact.tsx
@@ -7,9 +7,9 @@ export default function ButtonContact ({
 }:ButtonContactProps) {
     return(
         <div className="px-8">
-             <button className="p-3 text-lg font-semibold transition text-slate-100 rounded bg-lime-600 hover:bg-lime-500 hover:shadow-lg hover:shadow-lime-600/50 hover:-translate-y-0.5 flex items-center justify-center">
+             <button className="p-3 text-lg font-semibold transition text-slate-100 rounded bg-lime-600 hover:bg-lime-500 hover:shadow-lg hover:shadow-lime-600/50 hover:-translate-y-0.5 flex items-center justify-center select-none">
                 {icon}
-                <a href={link} target="_blank" className="mx-1">
+                <a href={link} target="_blank" className="mx-1 select-none">
                     {title}
                 </a>
             </button>
--- src/app/components/navbar/menuDropdown/MenuDropdown.tsx
+++ src/app/components/navbar/menuDropdown/MenuDropdown.tsx
@@ -9,13 +9,13 @@ import { randomUUID } from "crypto"
     return ( 
         <div className="w-full h-full flex flex-col mt-14 text-center">
             {data.titles.map((title, i) => (
-                <div key={uuid} className={`rounded drop-shadow-xl backdrop-blur-sm ${color} mb-10 py-3 px-5 hover:scale-110 transition ease-in-out delay-150`}>
-                    <h1 className="font-semibold text-slate-100 text-lg">
-                        <a href={data.links ?data.links[i]: '#'}>
-                            {title}
-                        </a>
-                    </h1>
-                </div>
+                 <a href={data.links ?data.links[i]: '#'}>
+                    <div key={uuid} className={`rounded drop-shadow-xl backdrop-blur-sm ${color} mb-10 py-3 px-5 hover:scale-110 transition ease-in-out delay-150`}>
+                        <h1 className="font-semibold text-slate-100 text-lg select-none">
+                                {title}  
+                        </h1>
+                    </div>
+                </a>
             ))}
         </div>
     );
