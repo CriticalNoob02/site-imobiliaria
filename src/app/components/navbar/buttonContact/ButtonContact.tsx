bec73074213ca0be9d604f107b25dd2edf994b2e
CriticalNoob02 <adrieldchavea20@gmail.com>
2023-08-17 16:15:42 -0300

    feat: apply padin in button

--- src/app/components/navbar/buttonContact/ButtonContact.tsx
+++ src/app/components/navbar/buttonContact/ButtonContact.tsx
@@ -6,7 +6,7 @@ export default function ButtonContact ({
     icon
 }:ButtonContactProps) {
     return(
-        <div>
+        <div className="px-8">
              <button className="p-3 text-lg font-semibold transition text-slate-100 rounded bg-lime-600 hover:bg-lime-500 hover:shadow-lg hover:shadow-lime-600/50 hover:-translate-y-0.5 flex items-center justify-center">
                 {icon}
                 <a href={link} target="_blank" className="mx-1">
