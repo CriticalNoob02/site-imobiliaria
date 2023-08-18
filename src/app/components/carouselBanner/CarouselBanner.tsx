a4e4fe17f054ba7f3c538ff28eaa0a07cfe2cc43
AdrielDevelop <adrieldchavea20@gmail.com>
2023-08-18 14:09:11 -0300

    feat: edit carrousel Componente

--- src/app/components/carouselBanner/CarouselBanner.tsx
+++ src/app/components/carouselBanner/CarouselBanner.tsx
@@ -1,12 +1,44 @@
+'use client'
+
+import { useState } from "react"
+import ArrowButton from "./arrowButton/ArrowButton"
+import Glass from "./glass/Glass"
+import CardBanner from "./cardBanner/CardBanner"
+
 export default function  CarouselBanner () {
+    const [count, setCount] = useState(0)
+
+    const SumCount = () => {
+        if(count == 3){
+            setCount(0)
+        }
+        else{
+            setCount(count + 1)
+        }
+    }
+    const LessCount = () => {
+        if(count == 0){
+            setCount(3)
+        }
+        else{
+            setCount(count - 1)
+        }
+    }
+
     return(
-        <div className="w-screen h-screen card">
-            <div className="backdrop-blur-sm bg-white-200/20 w-full h-full flex items-center justify-center">
-                <div className="overflow-hidden w-3/5 h-2/5 flex items-center justify-center shadow-2xl rounded outline outline-offset-1 outline-neutral-500/10">
-                    <div className="w-screen h-screen card">
+        <div className={`w-screen h-screen ${'card'+count} transition-all`}>
+            <Glass>
+                <CardBanner size="w-3/5 h-2/5">
+                    <div className={`w-screen h-screen ${'card'+count} transition-all`}>
                     </div>
-                </div>
-            </div>
+                    <div className="text-cente absolute right-0 px-10">
+                        <ArrowButton size="text-5xl" onClick={SumCount}/>
+                    </div>
+                    <div className="text-cente absolute left-0 px-10 -scale-x-100">
+                        <ArrowButton size="text-5xl" onClick={LessCount}/>
+                    </div>
+                </CardBanner>
+            </Glass>
         </div>
     )
 }
