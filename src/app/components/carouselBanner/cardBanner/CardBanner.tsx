ee3c6c863118ec1bee619162d1de20cd34d6e84a
AdrielDevelop <adrieldchavea20@gmail.com>
2023-08-18 13:54:37 -0300

    feat: creat CardBanner component

---
+++ src/app/components/carouselBanner/cardBanner/CardBanner.tsx
@@ -0,0 +1,9 @@
+import CardBannerProps from "./types";
+
+export default function CardBanner ({size,children}:CardBannerProps) {
+    return(
+        <div className={`${size} overflow-hidden flex items-center justify-center shadow-2xl rounded outline outline-offset-1 outline-neutral-500/10`}>
+            {children}
+        </div>
+    )
+}
---
+++ src/app/components/carouselBanner/cardBanner/types/index.ts
@@ -0,0 +1,8 @@
+/**
+ * @param children Componentes internos;
+ * @param size Tamanho do card;
+ */
+export default interface CardBannerProps {
+    children: React.ReactNode
+    size: string
+}
