1d81b4e2c12bcf4729e5536e8dab0b51fc03279c
AdrielDevelop <adrieldchavea20@gmail.com>
2023-08-18 19:59:53 -0300

    feat: add text in card

--- src/app/components/carouselBanner/CarouselBanner.tsx
+++ src/app/components/carouselBanner/CarouselBanner.tsx
@@ -4,9 +4,11 @@ import { useState } from "react"
 import ArrowButton from "./arrowButton/ArrowButton"
 import Glass from "./glass/Glass"
 import CardBanner from "./cardBanner/CardBanner"
+import TextLink from "./textLink/TextLink"
 
 export default function  CarouselBanner () {
     const [count, setCount] = useState(0)
+
 
     const SumCount = () => {
         if(count == 3){
@@ -25,17 +27,27 @@ export default function  CarouselBanner () {
         }
     }
 
+    const titles = ["Apartamento","Casa","Terreno","Sobrado"]
+    const links = ["#Apartamento","#Casa","#Terreno","#Sobrado"]
+
     return(
         <div className={`w-screen h-screen ${'card'+count} transition-all`}>
             <Glass>
                 <CardBanner size="w-3/5 h-2/5">
-                    <div className={`w-screen h-screen ${'card'+count} transition-all`}>
+                    <div className={`w-screen h-screen ${'card'+count} transition-all flex items-center flex-col justify-center `}>
+                        <h1 className="text-start font-light text-2xl my-5 mr-40 text-stone-300 select-none">Venha conquistar seu </h1>
+                        <TextLink title={titles[count]} link={links[count]}/>   
+                        <h1 className="text-end font-light text-2xl my-5 ml-40 text-stone-300 select-none">com a gente!</h1> 
                     </div>
-                    <div className="text-cente absolute right-0 px-10">
+                    <div className="text-cente absolute right-0 px-10 z-20">
                         <ArrowButton size="text-5xl" onClick={SumCount}/>
                     </div>
-                    <div className="text-cente absolute left-0 px-10 -scale-x-100">
+                    <div className="text-cente absolute left-0 px-10 -scale-x-100 z-20">
                         <ArrowButton size="text-5xl" onClick={LessCount}/>
+                    </div>
+                    <div className="h-screen w-28 absolute left-0 px-10 bg-gradient-to-r from-black/70 z-10">
+                    </div>
+                    <div className="h-screen w-28 absolute right-0 px-10 bg-gradient-to-l from-black/70 z-10">
                     </div>
                 </CardBanner>
             </Glass>
