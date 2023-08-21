7d25125e4e01bc95644d34ea0d5b0a06dd97ac99
CriticalNoob02 <adrieldchavea20@gmail.com>
2023-08-21 17:14:39 -0300

    refactor: change class name

--- src/app/components/cardServices/card3D/Card3D.tsx
+++ src/app/components/cardServices/card3D/Card3D.tsx
@@ -2,7 +2,7 @@ import Card3dProps from "./types";
 
 export default function Card3d ({children, color}:Card3dProps) {
     return(
-        <div className={`cardService group h-3/5 w-1/5 ${color} rounded p-5 mx-10 transition duration-300`}>
+        <div className={`card3D group h-3/5 w-1/5 ${color} rounded p-5 transition duration-300`}>
             {children}
         </div>
     )
