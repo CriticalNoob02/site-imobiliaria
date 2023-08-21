d7d5fb57709781e47d41839262a23305c76c673f
CriticalNoob02 <adrieldchavea20@gmail.com>
2023-08-21 17:09:44 -0300

    feat: create CardServices components

---
+++ src/app/components/cardServices/CardServices.tsx
@@ -0,0 +1,26 @@
+import Card3d from "./card3D/Card3D"
+import Gradient from "./gradient/Gradient"
+
+export default function CardServices() {
+    return(
+        <div className="w-screen h-[90vh] flex items-center bg-slate-100 justify-center">
+            <Card3d color="bg-red-800">
+                <div className="h-full w-full rounded bg-ImgCard1 bg-center bg-no-repeat relative flex items-end justify-center transition group-hover:grayscale">
+                    <h1 className="my-5 text-3xl tracking-wide font-bold text-slate-50 transition drop-shadow-md shadow-red-800 z-20">
+                        Trabalhe conosco!
+                    </h1>
+                    <Gradient/>
+                </div>
+            </Card3d>
+
+            <Card3d color="bg-red-800">
+                <div className="h-full w-full rounded bg-ImgCard2 bg-center bg-no-repeat relative flex items-end justify-center transition group-hover:grayscale">
+                    <h1 className="my-5 text-3xl tracking-wide font-bold text-slate-50 transition drop-shadow-md shadow-red-800 z-20">
+                        Realize seu sonho!
+                    </h1>
+                    <Gradient/>
+                </div>
+            </Card3d>
+        </div>
+    )
+}
