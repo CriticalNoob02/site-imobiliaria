a9e33986f390c2c6f5b0331864feda04eeff9869
AdrielDevelop <adrieldchavea20@gmail.com>
2023-08-21 18:28:01 -0300

    feat: add scrool in app

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="pt-BR">
+    <html lang="pt-BR" className='scroll-smooth'>
       <body className={inter.className}>
         <Base>
           <Navbar/>
