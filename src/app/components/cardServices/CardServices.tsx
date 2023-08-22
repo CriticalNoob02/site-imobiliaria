3e4f51fed353031e0a5c32cdf928117056af4851
CriticalNoob02 <adrieldchavea20@gmail.com>
2023-08-22 16:06:35 -0300

    feat: finished Card services component

--- src/app/components/cardServices/CardServices.tsx
+++ src/app/components/cardServices/CardServices.tsx
@@ -1,25 +1,37 @@
 import Card3d from "./card3D/Card3D"
 import Gradient from "./gradient/Gradient"
+import { PiHandshake, PiKey, PiHouse } from "react-icons/pi";
 
 export default function CardServices() {
     return(
         <div id="Services" className="w-screen h-[95vh] flex items-center justify-evenly flex-col bg-slate-100 text-center">
-            <h1 className="text-5xl text-red-800 font-bold py-10 drop-shadow-md">
+            <h1 className="text-6xl text-red-800 font-bold py-20 drop-shadow-md">
                 Serviços
             </h1>
             <div className="w-full h-full flex items-center justify-evenly flex-row">
                 <Card3d color="bg-red-800">
-                    <div className="h-full w-full rounded bg-ImgCard1 bg-center bg-no-repeat relative flex items-end justify-center transition group-hover:grayscale">
-                        <h1 className="my-5 text-3xl tracking-wide font-bold text-slate-50 transition drop-shadow-md shadow-red-800 z-20">
+                    <div className="h-full w-full rounded texture relative flex flex-col items-center transition">
+                        <PiHandshake className="transition text-[10rem] text-red-800 grow group-hover:drop-shadow-lg group-hover:scale-110"/>
+                        <h1 className="text-3xl my-4 tracking-wide font-bold text-slate-50 transition drop-shadow-md shadow-red-800 select-none z-20">
                             Trabalhe conosco!
                         </h1>
                         <Gradient/>
                     </div>
                 </Card3d>
                 <Card3d color="bg-red-800">
-                    <div className="h-full w-full rounded bg-ImgCard2 bg-center bg-no-repeat relative flex items-end justify-center transition group-hover:grayscale">
-                        <h1 className="my-5 text-3xl tracking-wide font-bold text-slate-50 transition drop-shadow-md shadow-red-800 z-20">
-                            Realize seu sonho!
+                    <div className="h-full w-full rounded texture relative flex flex-col items-center transition">
+                        <PiKey className="transition text-[10rem] text-red-800 grow group-hover:drop-shadow-lg group-hover:scale-110"/>
+                        <h1 className="text-3xl my-4 tracking-wide font-bold text-slate-50 transition drop-shadow-md shadow-red-800 select-none z-20">
+                            Conquiste seu imovel!
+                        </h1>
+                        <Gradient/>
+                    </div>
+                </Card3d>
+                <Card3d color="bg-red-800">
+                    <div className="h-full w-full rounded texture relative flex flex-col items-center transition">
+                        <PiHouse className="transition text-[10rem] text-red-800 grow group-hover:drop-shadow-lg group-hover:scale-110"/>
+                        <h1 className="text-3xl my-4 tracking-wide font-bold text-slate-50 transition drop-shadow-md shadow-red-800 select-none z-20">
+                            Venda seu Imóvel!
                         </h1>
                         <Gradient/>
                     </div>
