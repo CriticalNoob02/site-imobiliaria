a468ae76c4ba477c6d7dc6555d1933fe7cc76725
CriticalNoob02 <adrieldchavea20@gmail.com>
2023-08-22 08:54:06 -0300

    feat: add global css and component in page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import CarouselBanner from "./components/carouselBanner/CarouselBanner"
+import CardServices from "./components/cardServices/CardServices"
 
 export default function Home() {
   return (
     <main>
       <CarouselBanner/>
+      <CardServices/>
     </main>
   )
 }
