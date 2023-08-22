994a9dad83a6b50d0fb4ffeb8e60cbd848559c52
CriticalNoob02 <adrieldchavea20@gmail.com>
2023-08-22 17:33:41 -0300

    refactor: adjust height in gradient

--- src/app/components/cardServices/gradient/Gradient.tsx
+++ src/app/components/cardServices/gradient/Gradient.tsx
@@ -1,6 +1,6 @@
 export default function Gradient () {
     return (
-        <div className="h-1/5 w-full rounded  bg-gradient-to-t from-black absolute bottom-0 left-0">
+        <div className="h-2/6 w-full rounded transition bg-gradient-to-t from-black/80 absolute bottom-0 left-0">
         </div>
     )
 }
