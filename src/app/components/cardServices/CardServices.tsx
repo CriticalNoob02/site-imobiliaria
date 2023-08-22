7045b0b45d25d414779404d6b2bdb73457c3ed1a
CriticalNoob02 <adrieldchavea20@gmail.com>
2023-08-22 17:34:30 -0300

    refactor: change text and background in the Card

--- src/app/components/cardServices/CardServices.tsx
+++ src/app/components/cardServices/CardServices.tsx
@@ -4,36 +4,33 @@ import { PiHandshake, PiKey, PiHouse } from "react-icons/pi";
 
 export default function CardServices() {
     return(
-        <div id="Services" className="w-screen h-[95vh] flex items-center justify-evenly flex-col bg-slate-100 text-center">
-            <h1 className="text-6xl text-red-800 font-bold py-20 drop-shadow-md">
+        <div id="Services" className="w-screen h-[95vh] flex items-center justify-between flex-col bg-slate-100 text-center py-14">
+            <h1 className="text-5xl text-red-800 font-bold drop-shadow-md">
                 Serviços
             </h1>
             <div className="w-full h-full flex items-center justify-evenly flex-row">
                 <Card3d color="bg-red-800">
-                    <div className="h-full w-full rounded texture relative flex flex-col items-center transition">
-                        <PiHandshake className="transition text-[10rem] text-red-800 grow group-hover:drop-shadow-lg group-hover:scale-110"/>
-                        <h1 className="text-3xl my-4 tracking-wide font-bold text-slate-50 transition drop-shadow-md shadow-red-800 select-none z-20">
-                            Trabalhe conosco!
+                    <div className="h-full w-full rounded relative flex flex-col items-center transition">
+                        <PiHandshake className="transition text-[10rem] text-slate-50 grow group-hover:drop-shadow-lg group-hover:scale-110"/>
+                        <h1 className="text-2xl m-4 tracking-wide font-semibold text-slate-50 transition drop-shadow-md select-none z-20">
+                            Quero fazer parte da equipe de corretores!
                         </h1>
-                        <Gradient/>
                     </div>
                 </Card3d>
                 <Card3d color="bg-red-800">
-                    <div className="h-full w-full rounded texture relative flex flex-col items-center transition">
-                        <PiKey className="transition text-[10rem] text-red-800 grow group-hover:drop-shadow-lg group-hover:scale-110"/>
-                        <h1 className="text-3xl my-4 tracking-wide font-bold text-slate-50 transition drop-shadow-md shadow-red-800 select-none z-20">
-                            Conquiste seu imovel!
+                    <div className="h-full w-full rounded relative flex flex-col items-center transition">
+                        <PiKey className="transition text-[10rem] text-slate-50 grow group-hover:drop-shadow-lg group-hover:scale-110"/>
+                        <h1 className="text-2xl m-4 tracking-wide font-semibold text-slate-50 transition drop-shadow-md select-none z-20">
+                            Quero comprar meu imovel novo com vocês!
                         </h1>
-                        <Gradient/>
                     </div>
                 </Card3d>
                 <Card3d color="bg-red-800">
-                    <div className="h-full w-full rounded texture relative flex flex-col items-center transition">
-                        <PiHouse className="transition text-[10rem] text-red-800 grow group-hover:drop-shadow-lg group-hover:scale-110"/>
-                        <h1 className="text-3xl my-4 tracking-wide font-bold text-slate-50 transition drop-shadow-md shadow-red-800 select-none z-20">
-                            Venda seu Imóvel!
+                    <div className="h-full w-full rounded relative flex flex-col items-center transition">
+                        <PiHouse className="transition text-[10rem] text-slate-50 grow group-hover:drop-shadow-lg group-hover:scale-110"/>
+                        <h1 className="text-2xl m-4 tracking-wide font-semibold text-slate-50 transition drop-shadow-md select-none z-20">
+                            Quero vender meu imovel 
                         </h1>
-                        <Gradient/>
                     </div>
                 </Card3d>
             </div>
