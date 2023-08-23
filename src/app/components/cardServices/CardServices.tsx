b9e5c7c9912ce95d4fa5fc7bbad96e9ffb751408
CriticalNoob02 <adrieldchavea20@gmail.com>
2023-08-23 15:21:32 -0300

    feat: apply layout componentes in CardServices

--- src/app/components/cardServices/CardServices.tsx
+++ src/app/components/cardServices/CardServices.tsx
@@ -1,3 +1,5 @@
+import Row from '../layout/row/Row';
+import Col from '../layout/collum/Col';
 import Card3d from './card3D/Card3D';
 import IconContainer from './iconContainer/IconContainer';
 import Title from './title/Title';
@@ -11,30 +13,29 @@ export default function CardServices() {
   const tag = 'Serviços'
 
   return (
-    <div
-      id="Services"
-      className="flex h-[95vh] w-screen flex-col items-center justify-between bg-slate-100 py-14 text-center"
-    >
-      <Title
-      title={tag}
-      />
-      <div className="flex h-full w-full flex-row items-center justify-evenly">
-      {titles.map((title, i) => (
-          <Card3d
-          key={title}
-          color="bg-red-800"
-          >
-          <div className="relative flex h-full w-full flex-col items-center rounded transition">
-            <IconContainer>
-              {createElement(icons[i])}
-            </IconContainer>
-            <h1 className="z-20 m-4 select-none text-2xl font-semibold tracking-wide text-slate-50 drop-shadow-md transition">
-              {title}
-            </h1>
-          </div>
-        </Card3d>
-        ))}
+    <Col id='Services' height='h-[95vh]'>
+      <div className='my-16'>
+        <Title
+        title={tag}
+        />
       </div>
-    </div>
+      <Row width='w-full' height='h-full'>
+        {titles.map((title, i) => (
+            <Card3d
+            key={title}
+            color="bg-red-800"
+            >
+            <div className="relative flex h-full w-full flex-col items-center rounded transition">
+              <IconContainer>
+                {createElement(icons[i])}
+              </IconContainer>
+              <h1 className="z-20 m-4 select-none text-2xl font-semibold tracking-wide text-slate-50 drop-shadow-md transition">
+                {title}
+              </h1>
+            </div>
+          </Card3d>
+          ))}
+      </Row>
+    </Col>
   );
 }
