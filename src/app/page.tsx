7d7e5bdeefd0d1fd62b024c9fa486a6258c6617c
CriticalNoob02 <adrieldchavea20@gmail.com>
2023-08-24 14:10:42 -0300

    feat: add prettier plugin in eslint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import CarouselBanner from './components/carouselBanner/CarouselBanner';
 import CardServices from './components/cardServices/CardServices';
+import Review from './components/review/Review';
 
 export default function Home() {
   return (
     <main>
       <CarouselBanner />
       <CardServices />
+      <Review/>
     </main>
   );
 }
