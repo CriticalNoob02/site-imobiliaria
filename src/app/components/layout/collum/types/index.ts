43b42191af4371bf2bf504677a57eb67d09bf9e9
CriticalNoob02 <adrieldchavea20@gmail.com>
2023-08-25 16:52:33 -0300

    feat: add id in Col

--- src/app/components/layout/collum/types/index.ts
+++ src/app/components/layout/collum/types/index.ts
@@ -3,11 +3,12 @@
  * @param width <?> Largura do componente;
  * @param height <?> Altura do componente;
  * @param color <?> Cor de fundo do componente;
+ * @param id <?> ID se do componente;
  */
 export default interface ColProps {
   children: React.ReactNode;
   width?: string;
   height?: string;
   color?: string;
-  id?: string
+  id?: string;
 }
