e8c5755111ea067122442570d6d681bec7b4066f
CriticalNoob02 <adrieldchavea20@gmail.com>
2023-08-25 16:45:06 -0300

    feat: add rest in layoute componentes

--- src/app/components/layout/collum/Col.tsx
+++ src/app/components/layout/collum/Col.tsx
@@ -1,15 +1,16 @@
 import ColProps from "./types";
 
 export default function Col ({
-  children,
-  id,
-  width = 'w-screen',
-  height = 'h-screen',
-  color = "bg-slate-100",
+    children,
+    id,
+    width = 'w-screen',
+    height = 'h-screen',
+    color = "bg-slate-100",
+    ...rest
 }:ColProps) {
-  return(
-    <div id={id ?? ''} className={`${width} ${height} ${color} flex justify-center items-center flex-col text-center py-5`}>
-      {children}
-    </div>
-  )
+    return(
+        <div id={id ?? ''} className={`${width} ${height} ${color} flex justify-center items-center flex-col text-center py-5 ${{...rest}}`}>
+            {children}
+        </div>
+    )
 }
--- src/app/components/layout/row/Row.tsx
+++ src/app/components/layout/row/Row.tsx
@@ -5,10 +5,11 @@ export default function Row ({
     width = 'w-screen',
     height = 'h-screen',
     padding,
-    color = "bg-slate-100"
+    color = "bg-slate-100",
+    ...rest
 }:RowProps) {
     return(
-        <div className={`${width} ${height} ${color} ${padding ? 'py-10' : ''} flex justify-evenly items-center flex-row`}>
+        <div className={`${width} ${height} ${color} ${padding ? 'py-10' : ''} flex justify-evenly items-center flex-row ${{...rest}}`}>
             {children}
         </div>
     )
