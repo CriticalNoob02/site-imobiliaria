b736fa89d18fbdadc405168a3b8ead8f42b64148
CriticalNoob02 <adrieldchavea20@gmail.com>
2023-08-25 08:51:52 -0300

    refactor: apply identation

--- src/app/components/carouselBanner/CarouselBanner.tsx
+++ src/app/components/carouselBanner/CarouselBanner.tsx
@@ -7,59 +7,59 @@ import CardBanner from './cardBanner/CardBanner';
 import TextLink from './textLink/TextLink';
 
 export default function CarouselBanner() {
-  const [count, setCount] = useState(0);
+    const [count, setCount] = useState(0);
 
-  const increment = () => {
-    if (count == 3) {
-      setCount(0);
-    } else {
-      setCount(count + 1);
-    }
-  };
-  const decrement = () => {
-    if (count == 0) {
-      setCount(3);
-    } else {
-      setCount(count - 1);
-    }
-  };
+    const increment = () => {
+        if (count == 3) {
+            setCount(0);
+        } else {
+            setCount(count + 1);
+        }
+    };
+    const decrement = () => {
+        if (count == 0) {
+            setCount(3);
+        } else {
+            setCount(count - 1);
+        }
+    };
 
-  useEffect(() => {
-    const timer = setTimeout(() => increment(), 4000);
-    return () => clearTimeout(timer);
-  }, [count,4]);
+    useEffect(() => {
+        const timer = setTimeout(() => increment(), 4000);
+        return () => clearTimeout(timer);
+    }, [count,4]);
 
-  const titles = ['Apartamento', 'Casa', 'Terreno', 'Sobrado'];
-  const links = ['#Apartamento', '#Casa', '#Terreno', '#Sobrado'];
+    const titles = ['Apartamento', 'Casa', 'Terreno', 'Sobrado'];
+    const links = ['#Apartamento', '#Casa', '#Terreno', '#Sobrado'];
 
-  return (
-    <div
-      id="Home"
-      className={`h-screen w-screen ${'card' + count} transition-all`}
-    >
-      <Glass>
-        <CardBanner size="w-3/5 h-2/5">
-          <div
-            className={`h-screen w-screen ${'card' + count} flex flex-col items-center justify-center transition-all`}
-          >
-            <h1 className="my-5 mr-40 select-none text-start text-2xl font-light text-stone-300">
+    return (
+        <div
+            id="Home"
+            className={`h-screen w-screen ${'card' + count} transition-all`}
+        >
+            <Glass>
+                <CardBanner size="w-3/5 h-2/5">
+                    <div
+                        className={`h-screen w-screen ${'card' + count} flex flex-col items-center justify-center transition-all`}
+                    >
+                        <h1 className="my-5 mr-40 select-none text-start text-2xl font-light text-stone-300">
               Venha conquistar seu
-            </h1>
-            <TextLink title={titles[count]} link={links[count]} />
-            <h1 className="my-5 ml-40 select-none text-end text-2xl font-light text-stone-300">
+                        </h1>
+                        <TextLink title={titles[count]} link={links[count]} />
+                        <h1 className="my-5 ml-40 select-none text-end text-2xl font-light text-stone-300">
               com a gente!
-            </h1>
-          </div>
-          <div className="text-cente absolute right-0 z-20 px-10">
-            <ArrowButton size="text-5xl" onClick={increment} />
-          </div>
-          <div className="text-cente absolute left-0 z-20 -scale-x-100 px-10">
-            <ArrowButton size="text-5xl" onClick={decrement} />
-          </div>
-          <div className="absolute left-0 z-10 h-screen w-28 bg-gradient-to-r from-black/70 px-10"></div>
-          <div className="absolute right-0 z-10 h-screen w-28 bg-gradient-to-l from-black/70 px-10"></div>
-        </CardBanner>
-      </Glass>
-    </div>
-  );
+                        </h1>
+                    </div>
+                    <div className="text-cente absolute right-0 z-20 px-10">
+                        <ArrowButton size="text-5xl" onClick={increment} />
+                    </div>
+                    <div className="text-cente absolute left-0 z-20 -scale-x-100 px-10">
+                        <ArrowButton size="text-5xl" onClick={decrement} />
+                    </div>
+                    <div className="absolute left-0 z-10 h-screen w-28 bg-gradient-to-r from-black/70 px-10"></div>
+                    <div className="absolute right-0 z-10 h-screen w-28 bg-gradient-to-l from-black/70 px-10"></div>
+                </CardBanner>
+            </Glass>
+        </div>
+    );
 }
--- src/app/components/navbar/Navbar.tsx
+++ src/app/components/navbar/Navbar.tsx
@@ -10,47 +10,47 @@ import ButtonMenu from './buttonMenu/ButtonMenu';
 import MenuDropdown from './menuDropdown/MenuDropdown';
 
 export default function Navbar() {
-  const title = ['Home', 'Serviços', 'CCC', 'Jabiraca'];
-  const link = ['#Home', '#Services', '#CCC', '#AAcc'];
-  const buttonTitle = 'Contato';
-  const buttonLink =
+    const title = ['Home', 'Serviços', 'CCC', 'Jabiraca'];
+    const link = ['#Home', '#Services', '#CCC', '#AAcc'];
+    const buttonTitle = 'Contato';
+    const buttonLink =
     'https://api.whatsapp.com/send?phone=5548991539040&text=Ol%C3%A1,%20estou%20visitando%20o%20site,%20gostaria%20de%20saber%20mais%20informa%C3%A7%C3%B5es';
 
-  const [menuStats, setMenuStats] = useState(true);
+    const [menuStats, setMenuStats] = useState(true);
 
-  const ToogleMode = () => {
-    setMenuStats(!menuStats);
-  };
-  return (
-    <div className="fixed z-40 flex h-28 w-screen justify-around bg-white/20 p-2 px-28 drop-shadow-xl backdrop-blur-sm">
-      <div className="basis-1/5 overflow-hidden">
-        <Image src={logo} height={95} alt="logo da empresa" />
-      </div>
-      <div className="basis-4/5">
-        <div className="flex h-full w-full items-center justify-end">
-          <ButtonContact
-            link={buttonLink}
-            title={buttonTitle}
-            icon={<BsWhatsapp />}
-          />
-          <ButtonMenu status={menuStats} onClick={ToogleMode} />
-          <div
-            className={`${
-              menuStats
-                ? 'invisible top-10 opacity-0'
-                : 'visible top-20 opacity-100'
-            } absolute right-auto transition-all`}
-          >
-            <MenuDropdown
-              data={{
-                titles: title,
-                links: link,
-              }}
-              color="bg-red-800/70"
-            />
-          </div>
+    const ToogleMode = () => {
+        setMenuStats(!menuStats);
+    };
+    return (
+        <div className="fixed z-40 flex h-28 w-screen justify-around bg-white/20 p-2 px-28 drop-shadow-xl backdrop-blur-sm">
+            <div className="basis-1/5 overflow-hidden">
+                <Image src={logo} height={95} alt="logo da empresa" />
+            </div>
+            <div className="basis-4/5">
+                <div className="flex h-full w-full items-center justify-end">
+                    <ButtonContact
+                        link={buttonLink}
+                        title={buttonTitle}
+                        icon={<BsWhatsapp />}
+                    />
+                    <ButtonMenu status={menuStats} onClick={ToogleMode} />
+                    <div
+                        className={`${
+                            menuStats
+                                ? 'invisible top-10 opacity-0'
+                                : 'visible top-20 opacity-100'
+                        } absolute right-auto transition-all`}
+                    >
+                        <MenuDropdown
+                            data={{
+                                titles: title,
+                                links: link,
+                            }}
+                            color="bg-red-800/70"
+                        />
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
