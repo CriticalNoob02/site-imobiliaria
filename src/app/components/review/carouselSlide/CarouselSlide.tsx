77029ab8fd94c9d351a78411f041fe11291ccdfa
AdrielDevelop <adrieldchavea20@gmail.com>
2023-08-25 22:26:24 -0300

    refactor: remove comments and add use-selector

--- src/app/components/review/carouselSlide/CarouselSlide.tsx
+++ src/app/components/review/carouselSlide/CarouselSlide.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 
 export default function CarouselSlide ({images,timer}:CarouselSlideProps) {
     return(
-        <div className={`h-full flex flex-row items-center justify-start flex-nowrap animate-[slides_20s_linear_infinite]`}>
+        <div className={`h-full flex flex-row items-center justify-start flex-nowrap animate-[slides_20s_linear_infinite] select-none`}>
             {images.map((image,i) => (
                 <div key={image.toString()} className="w-full h-full shrink-0">
                     <Image
@@ -30,42 +30,3 @@ export default function CarouselSlide ({images,timer}:CarouselSlideProps) {
         </div>
     )
 }
-
-
-{/* <div className="w-full h-full bg-red-400 shrink-0">
-                <h1>1</h1>
-            </div>
-            <div className="w-full h-full bg-red-300 shrink-0">
-                <h1>2</h1>
-            </div>
-            <div className="w-full h-full bg-red-500 shrink-0">
-                <h1>3</h1>
-            </div>
-            <div className="w-full h-full bg-red-600 shrink-0">
-                <h1>4</h1>
-            </div>
-            <div className="w-full h-full bg-red-700 shrink-0">
-                <h1>5</h1>
-            </div>
-            <div className="w-full h-full bg-red-900 shrink-0">
-                <h1>6</h1>
-            </div>
-
-            <div className="w-full h-full bg-red-400 shrink-0">
-                <h1>1</h1>
-            </div>
-            <div className="w-full h-full bg-red-300 shrink-0">
-                <h1>2</h1>
-            </div>
-            <div className="w-full h-full bg-red-500 shrink-0">
-                <h1>3</h1>
-            </div>
-            <div className="w-full h-full bg-red-600 shrink-0">
-                <h1>4</h1>
-            </div>
-            <div className="w-full h-full bg-red-700 shrink-0">
-                <h1>5</h1>
-            </div>
-            <div className="w-full h-full bg-red-900 shrink-0">
-                <h1>6</h1>
-            </div> */}
