d25897048a0c7bfac8bb0b6e122942289c661d4c
CriticalNoob02 <adrieldchavea20@gmail.com>
2023-08-28 18:09:59 -0300

    feat: add count function in ReviewCard

--- src/app/components/review/reviewCard/ReviewCard.tsx
+++ src/app/components/review/reviewCard/ReviewCard.tsx
@@ -1,9 +1,18 @@
+import { useState } from "react"
 import ReviewCardProps from "./types"
 import Avatar from "../avatar/Avatar"
 import ArrowButton from "../../carouselBanner/arrowButton/ArrowButton"
 
 
 export default function ReviewCard({name, img, comment, rating}:ReviewCardProps) {
+    const [count,setCount] = useState(0)
+    const increment = () => {
+        count == name.length ? null : setCount(+1)
+    }
+    const decrement = () => {
+        count == 0 ? null : setCount(+1)
+    }
+
     return(
         <div className="w-full h-full flex items-center flex-row flex-nowrap shrink-0 overflow-hidden">
             {name.map((n,i) => (
@@ -27,10 +36,10 @@ export default function ReviewCard({name, img, comment, rating}:ReviewCardProps)
                 </div>
             ))}
             <div className="text-cente absolute right-0 z-20 px-24">
-                <ArrowButton size="text-5xl" color="text-red-800"/>
+                <ArrowButton size="text-5xl" color="text-red-800" onClick={increment}/>
             </div>
             <div className="text-cente absolute left-0 z-20 -scale-x-100 px-24">
-                <ArrowButton size="text-5xl" color="text-red-800"/>
+                <ArrowButton size="text-5xl" color="text-red-800" onClick={decrement}/>
             </div>
         </div>
     )
