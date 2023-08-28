b2e897242236679d7febe0928648567a2d4b8b28
CriticalNoob02 <adrieldchavea20@gmail.com>
2023-08-28 16:34:07 -0300

    feat: add size in component

--- src/app/components/review/startBar/StarBar.tsx
+++ src/app/components/review/startBar/StarBar.tsx
@@ -1,20 +1,23 @@
 import StarBarProps from "./types"
 import { PiStarFill, PiStar } from "react-icons/pi"
 
-export default function StarBar ({rate}:StarBarProps){
+export default function StarBar ({
+    rate,
+    size
+}:StarBarProps){
     const starsLoop = [1,2,3,4,5]
     const generateStars = () => {
         return starsLoop.map((i) => {
             return i <= rate ? (
-                <PiStarFill key={i} />
+                <PiStarFill key={i+'star'}/>
             ) : (
-                <PiStar key={i} />
+                <PiStar key={i+'star'}/>
             );
         });
     };
 
     return(
-        <div className="w-auto h-auto bg-white shadow-md rounded-md p-2 mx-4 flex flex-row text-yellow-400">
+        <div className={`w-auto h-auto bg-white shadow-md rounded-md p-2 mx-4 flex flex-row text-yellow-400 ${size}`}>
             {generateStars()}
         </div>
     )
--- src/app/components/review/startBar/types/index.ts
+++ src/app/components/review/startBar/types/index.ts
@@ -1,6 +1,8 @@
 /**
- * @param rate Valor da avaliação;
+ * @param rate valor da avaliação;
+ * @param size tamanho do elemento;
  */
 export default interface StarBarProps{
   rate: number;
+  size: string;
 }
