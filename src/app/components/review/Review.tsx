60d751b91b04477e04b1045654019458677cae92
CriticalNoob02 <adrieldchavea20@gmail.com>
2023-08-28 16:40:38 -0300

    feat: add ReviedCard in revied component

--- src/app/components/review/Review.tsx
+++ src/app/components/review/Review.tsx
@@ -2,11 +2,15 @@ import Col from "../layout/collum/Col"
 import Row from "../layout/row/Row"
 import Title from "../cardServices/title/Title"
 import Avatar from "./avatar/Avatar"
+import PostLayout from "./postLayout/PostLayout"
+import CarouselSlide from "./carouselSlide/CarouselSlide"
+import ReviewCard from "./reviewCard/ReviewCard"
+import StarBar from "./startBar/StarBar"
+
+
 import logo from "../../../../public/logo.png"
 import asd from "../../../../public/house.jpg"
 import zzx from "../../../../public/sobrado.jpg"
-import PostLayout from "./postLayout/PostLayout"
-import CarouselSlide from "./carouselSlide/CarouselSlide"
 
 export default function Review () {
 
@@ -16,18 +20,33 @@ export default function Review () {
     const images = [logo,asd,zzx]
     const time = '10s'
 
+    const name = 'Adriel taradinho!'
+    const userAvatar = asd
+    const comment = 'fgndjngnfgfngngdfngngjfgfigfgn'
+    const rating = <StarBar rate={4} size={""}/>
+
     return(
-        <Col id="Reviews" width="w-screen" height="h-[95vh]">
-            <div>
-                <Title title={title}/>
-            </div>
-            <Row width="w-full" height="h-full" color="bg-red-800" padding>
-                <PostLayout
-                    avatar={userLogo}
-                    username={username}
-                    posts={<CarouselSlide images={images} timer={time}/>}
+        <Col id="Reviews" height="v-auto">
+            <Col height="h-[95vh]">
+                <div>
+                    <Title title={title}/>
+                </div>
+                <Row width="w-full" height="h-full" color="bg-red-800" padding>
+                    <PostLayout
+                        avatar={userLogo}
+                        username={username}
+                        posts={<CarouselSlide images={images} timer={time}/>}
+                    />
+                </Row>
+            </Col>
+            <Col height="h-[50vh]">
+                <ReviewCard
+                    name={name}
+                    img={userAvatar}
+                    comment={comment}
+                    rating={rating}
                 />
-            </Row>
+            </Col>
         </Col>
     )
 }
