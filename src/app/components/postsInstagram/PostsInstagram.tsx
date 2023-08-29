e9bab82bfbbe4b78d211e96b0371936440289b6e
CriticalNoob02 <adrieldchavea20@gmail.com>
2023-08-29 13:57:26 -0300

    feat: change post element in new component

---
+++ src/app/components/postsInstagram/PostsInstagram.tsx
@@ -0,0 +1,40 @@
+import Row from "../layout/row/Row";
+import IconContainer from "../cardServices/iconContainer/IconContainer";
+import Avatar from "./avatar/Avatar";
+import CarouselSlide from "./carouselSlide/CarouselSlide";
+import { PiQuotes } from "react-icons/pi";
+import PostLayout from "./postLayout/PostLayout";
+
+import logo from "../../../../public/logo.png"
+import asd from "../../../../public/apartament.jpeg"
+import zzx from "../../../../public/terreno.jpg"
+
+export default function PostsInstagram() {
+
+    const title = 'Avaliações'
+    const username = '@ChavesNovas'
+    const userLogo = <Avatar image={logo}/>
+    const images = [logo,asd,zzx]
+    const time = '10s'
+
+    return(
+        <Row height="h-[80vh]" color="bg-red-800" padding>
+            <div className="w-2/6 h-full flex justify-center items-center text-slate-50 select-none">
+                <div className="-scale-x-100">
+                    <IconContainer size="text-8xl">
+                        <PiQuotes />
+                    </IconContainer>
+                </div>
+                <h1 className="text-3xl my-8">
+                    <i>Nossa melhor <b>obra de arte</b> é ver o sorriso no rosto de um <b>cliente Satisfeito!</b></i>
+                </h1>
+            </div>
+            <div className="w-2/6 h-full p-8">
+                <PostLayout
+                    avatar={userLogo}
+                    username={username}
+                    posts={<CarouselSlide images={images} timer={time} />}/>
+            </div>
+        </Row>
+    )
+}
