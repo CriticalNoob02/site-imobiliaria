0df2b560daad7db2ad19f448c1801621a1887708
CriticalNoob02 <adrieldchavea20@gmail.com>
2023-08-29 17:36:19 -0300

    refactor: remove imports

--- src/app/components/review/Review.tsx
+++ src/app/components/review/Review.tsx
@@ -1,13 +1,6 @@
 import Col from "../layout/collum/Col"
-import Row from "../layout/row/Row"
-import Title from "../cardServices/title/Title"
-import PostLayout from "../postsInstagram/postLayout/PostLayout"
-import CarouselSlide from "../postsInstagram/carouselSlide/CarouselSlide"
 import ReviewCard from "./reviewCard/ReviewCard"
 import StarBar from "./startBar/StarBar"
-import IconContainer from "../cardServices/iconContainer/IconContainer"
-import { PiQuotes } from "react-icons/pi"
-
 
 import asd from "../../../../public/house.jpg"
 import zzx from "../../../../public/sobrado.jpg"
