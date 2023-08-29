0e1850385a7ee2a55e8f93baa48a49b43ec9b449
CriticalNoob02 <adrieldchavea20@gmail.com>
2023-08-29 13:56:04 -0300

    feat: add PostInstagram in page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
 import CarouselBanner from './components/carouselBanner/CarouselBanner';
 import CardServices from './components/cardServices/CardServices';
 import Review from './components/review/Review';
+import PostsInstagram from './components/postsInstagram/PostsInstagram';
 
 export default function Home() {
-  return (
-    <main>
-      <CarouselBanner />
-      <CardServices />
-      <Review/>
-    </main>
-  );
+    return (
+        <main>
+            <CarouselBanner />
+            <CardServices />
+            <Review/>
+            <PostsInstagram/>
+        </main>
+    );
 }
