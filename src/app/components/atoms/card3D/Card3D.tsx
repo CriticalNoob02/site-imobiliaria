560f53ca252558875a7c1d9bf949a6b9db38bb2f
CriticalNoob02 <adrieldchavea20@gmail.com>
2023-08-30 11:40:26 -0300

    doc: add doc in Card3D

--- src/app/components/atoms/card3D/Card3D.tsx
+++ src/app/components/atoms/card3D/Card3D.tsx
@@ -1,5 +1,11 @@
 import Card3dProps from './types';
 
+/**
+ * Card com movimento 3D
+ * @param children Componente interno do Card;
+ * @param color Cor de fundo do Card (utilizar cores de Background);
+ * Tamanho do elemento deve ser definido no seu elemento pai;
+ */
 export default function Card3d({ children, color }: Card3dProps) {
     return (
         <div
