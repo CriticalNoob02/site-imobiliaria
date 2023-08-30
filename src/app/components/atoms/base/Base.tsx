3dfbc25c1d3c819bd4c0cd93398c59f5480cd8ff
AdrielDevelop <adrieldchavea20@gmail.com>
2023-08-29 21:01:15 -0300

    doc: add tsDocs in Base

--- src/app/components/atoms/base/Base.tsx
+++ src/app/components/atoms/base/Base.tsx
@@ -1,5 +1,8 @@
 import BaseProps from './types';
 
+/**
+ * Componente com tamanho da tela e display flex;
+ */
 export default function Base({ children }: BaseProps) {
     return (
         <div className=" h-auto min-h-screen w-screen flex-col items-center">
