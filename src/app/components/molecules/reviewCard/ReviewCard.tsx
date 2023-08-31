d9563da09a741dfa9fb34adabeb68d28d831ab56
AdrielDevelop <adrieldchavea20@gmail.com>
2023-08-30 21:28:51 -0300

    refactor: change ReviewCard from molecules

--- src/app/components/molecules/reviewCard/ReviewCard.tsx
+++ src/app/components/molecules/reviewCard/ReviewCard.tsx
@@ -1,22 +1,17 @@
-'use client';
-
-import { useState } from "react"
 import ReviewCardProps from "./types"
 import Avatar from "../../atoms/avatar/Avatar"
-import ArrowButton from "../../atoms/arrowButton/ArrowButton"
 
 
-export default function ReviewCard({name, img, comment, rating}:ReviewCardProps) {
-    const [count,setCount] = useState(0)
-    const increment = () => {
-        count == name.length ? null : setCount(+1)
-    }
-    const decrement = () => {
-        count == 0 ? null : setCount(+1)
-    }
+export default function ReviewCard({
+    name, 
+    img, 
+    comment, 
+    rating
+}:ReviewCardProps) {
+
 
     return(
-        <div className="w-full h-full flex items-center flex-row flex-nowrap shrink-0 overflow-hidden">
+        <div className="w-screen h-full flex flex-row">
             {name.map((n,i) => (
                 <div key={'avaliacao'+ i} className="h-full w-full flex flex-col flex-nowrap shrink-0 items-center justify-center">
                     <div className="w-3/5 h-1/4 flex flex-row items-center justify-center">
@@ -37,12 +32,6 @@ export default function ReviewCard({name, img, comment, rating}:ReviewCardProps)
                     </div>
                 </div>
             ))}
-            <div className="text-cente absolute right-0 z-20 px-24">
-                <ArrowButton size="text-5xl" color="text-red-800" onClick={increment}/>
-            </div>
-            <div className="text-cente absolute left-0 z-20 -scale-x-100 px-24">
-                <ArrowButton size="text-5xl" color="text-red-800" onClick={decrement}/>
-            </div>
         </div>
     )
 }
