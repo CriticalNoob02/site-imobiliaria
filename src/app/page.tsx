484dfb5fdf85ed3c908e242c2782e869f2ea5135
AdrielDevelop <adrieldchavea20@gmail.com>
2023-09-03 14:23:04 -0300

    feat: add PostInstagram in page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import CarouselBanner from './components/carouselBanner/CarouselBanner';
-import PostsInstagram from './components/postsInstagram/PostsInstagram';
+import PostsInstagram from './components/organisms/postsInstagram/PostsInstagram';
 import { Navbar, CardServices, ReviewCarousel } from './components/organisms';
 import { StarBar } from "./components/atoms"
 import asd from "../../public/house.jpg"
@@ -8,12 +8,12 @@ import logo from "../../public/logo.png"
 import { PiHandshake, PiKey, PiHouse } from 'react-icons/pi';
 
 export default function Home() {
-    //Navbar;
+    // Navbar;
     const title = ['Home', 'Serviços', 'Avaliações', 'Jabiraca'];
     const link = ['#Home', '#Services', '#Reviews', '#AAcc'];
     const buttonTitle = 'Contato';
     const buttonLink ='https://api.whatsapp.com/send?phone=5548991539040&text=Ol%C3%A1,%20estou%20visitando%20o%20site,%20gostaria%20de%20saber%20mais%20informa%C3%A7%C3%B5es';
-    //CardServices;
+    // CardServices;
     const titles = ['Quero fazer parte da equipe de corretores!','Quero comprar meu imovel novo com vocês!','Quero anunciar meu imovel com vocês!']
     const icons = [PiHandshake , PiKey, PiHouse]
     const tag = 'Serviços'
@@ -21,16 +21,19 @@ export default function Home() {
     const name = ['Adriel taradinho!', 'Crinha aleatório']
     const userAvatar = [asd, zzx]
     const comment = ['fgndjngnfgfngngdfngngjfgfigfgn', 'o outro cara disse tudo!']
-    // eslint-disable-next-line react/jsx-key
     const rating = [<StarBar rate={4} size={""}/>, <StarBar rate={2} size={""}/>]
+    // PostInstagram
+    const userLogo = logo
+    const images = [logo,asd,zzx]
+    const time = '10s'
 
     return (
         <main>
             <Navbar titles={title} links={link} buttonTitle={buttonTitle} buttonLink={buttonLink} logo={logo}/>
             <CarouselBanner />
-            <CardServices titles={titles} icons={icons} tag={tag} />
             <ReviewCarousel names={name} imgs={userAvatar} comments={comment} ratings={rating}/>
-            <PostsInstagram/>
+            <PostsInstagram userLogo={userLogo} images={images} time={time}/>
+            <CardServices titles={titles} icons={icons} tag={tag} />
         </main>
     );
 }
