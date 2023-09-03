c933bb83829510ec58f170bb21f5687675f668e5
AdrielDevelop <adrieldchavea20@gmail.com>
2023-09-02 23:07:09 -0300

    refactor: change PostLayoute for molecules

--- src/app/components/molecules/index.ts
+++ src/app/components/molecules/index.ts
@@ -2,10 +2,12 @@ import MenuDropdown from "./menuDropdown/MenuDropdown";
 import ButtonZap from "./buttonContact/ButtonZap";
 import Cards3D from "./cards3D/Cards3D";
 import ReviewCard from "./reviewCard/ReviewCard";
+import PostLayout from "./postLayout/PostLayout";
 
 export {
     MenuDropdown,
     ButtonZap,
     Cards3D,
     ReviewCard,
+    PostLayout,
 }
