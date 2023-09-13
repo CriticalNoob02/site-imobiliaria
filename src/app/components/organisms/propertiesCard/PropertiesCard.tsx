8952f3743c5af700436bd166b92d7036c3508a66
CriticalNoob02 <adrieldchavea20@gmail.com>
2023-09-13 18:12:16 -0300

    feat: add click event in slider Cards

--- src/app/components/organisms/propertiesCard/PropertiesCard.tsx
+++ src/app/components/organisms/propertiesCard/PropertiesCard.tsx
@@ -1,8 +1,21 @@
-import { Col, Title } from "../../atoms"
+'use client';
+
+
+import { useState } from "react"
+import { Col, Title, ArrowButton } from "../../atoms"
 import { CardsHover } from "../../molecules"
 import pics from "../../../../../public/sobrado.jpg"
 
 export default function PropertiesCard () {
+
+    const [ count, setCount ] = useState(0)
+
+    const increment = () => {
+        count == images.length ? null : setCount(count + 1);console.log(count)
+    }
+    const decrement = () => {
+        count == 0 ? null : setCount(count - 1);console.log(count)
+    }
 
     const images = [pics,pics,pics,pics,pics,pics]
     const titles = ['casa','abelha', 'casa','abelha','jonas','outro']
@@ -15,13 +28,25 @@ export default function PropertiesCard () {
                 <Title title="Lançamentos"/>
             </div>
             <div className="w-full h-4/5 bg-red-800 flex flex-row items-center justify-start flex-nowrap shrink-0 overflow-hidden">
-                <CardsHover
-                    images={images}
-                    imageTitles={titles}
-                    imageValues={values}
-                    imageTopics={topics}
-                />
+                <div className={`transition`}
+                    style={{
+                        transform: `translateX(-${24*count}rem)`
+                    }}>
+                    <CardsHover
+                        images={images}
+                        imageTitles={titles}
+                        imageValues={values}
+                        imageTopics={topics}
+                    />
+                </div>
+                <div className="text-cente absolute right-0 z-20 px-24">
+                    <ArrowButton size="text-5xl" color="text-red-800" onClick={increment}/>
+                </div>
+                <div className="text-cente absolute left-0 z-20 -scale-x-100 px-24">
+                    <ArrowButton size="text-5xl" color="text-red-800" onClick={decrement}/>
+                </div>
             </div>
         </Col>
     )
 }
+
