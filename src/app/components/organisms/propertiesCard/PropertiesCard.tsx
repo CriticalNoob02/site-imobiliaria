833178e39c0b805ff79af9eb925bdf613d9b627b
CriticalNoob02 <adrieldchavea20@gmail.com>
2023-09-15 14:10:00 -0300

    feat: add state from cards width and drag legend

--- src/app/components/organisms/propertiesCard/PropertiesCard.tsx
+++ src/app/components/organisms/propertiesCard/PropertiesCard.tsx
@@ -1,15 +1,28 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 'use client';
 
-import { useRef } from "react"
+import { useRef, useState, useEffect } from "react"
 import { Col, Title } from "../../atoms"
 import { CardsHover } from "../../molecules"
 import { motion } from "framer-motion"
+import { PiArrowFatLinesRight } from "react-icons/pi";
 import pics from "../../../../../public/sobrado.jpg"
 
 export default function PropertiesCard () {
 
     const cardsBox = useRef<HTMLDivElement>(null);
+
+    const [dragConstraints, setDrag] = useState(Object)
+
+    useEffect(() =>{
+        const newsDrag = {
+            right: 0,
+            left: cardsBox.current
+                ? -cardsBox.current.offsetWidth + window.innerWidth
+                : 0
+        };
+        setDrag(newsDrag)
+    },[])
 
 
     const images = [pics,pics,pics,pics,pics,pics]
@@ -22,8 +35,8 @@ export default function PropertiesCard () {
             <div className="w-full h-1/5">
                 <Title title={'Lançamentos'}/>
             </div>
-            <div className="w-screen h-4/5 bg-red-800 flex flex-row items-center justify-start flex-nowrap shrink-0 overflow-hidden">
-                <motion.div ref={cardsBox} drag="x" dragConstraints={{right: 0, left: -cardsBox.current?.offsetWidth + window.innerWidth}} className="cursor-grab active:cursor-grabbing">
+            <div className="w-screen h-4/5 bg-red-800 flex flex-row items-center justify-start flex-nowrap shrink-0 overflow-hidden relative">
+                <motion.div ref={cardsBox} drag="x" dragConstraints={dragConstraints} className="cursor-grab active:cursor-grabbing">
                     <CardsHover
                         images={images}
                         imageTitles={titles}
@@ -31,6 +44,12 @@ export default function PropertiesCard () {
                         imageTopics={topics}
                     />
                 </motion.div>
+                <div className="absolute right-0 bottom-0  px-20 pb-10 text-slate-100 text-5xl">
+                    <PiArrowFatLinesRight/>
+                </div>
+                <div className="absolute left-0 bottom-0 px-20 pb-10 text-slate-100 text-lg">
+                    <h1>Arraste para ver mais...</h1>
+                </div>
             </div>
         </Col>
     )
