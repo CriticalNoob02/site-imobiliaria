e3c9efcefd895ce7cfde9ddf31b89c84ab38a95d
AdrielDevelop <adrieldchavea20@gmail.com>
2023-09-29 21:47:07 -0300

    feat: add bg in text

--- src/app/components/organisms/carouselBanner/CarouselBanner.tsx
+++ src/app/components/organisms/carouselBanner/CarouselBanner.tsx
@@ -36,12 +36,12 @@ export default function CarouselBanner() {
             <Glass>
                 <CardBanner size="w-3/5 h-2/5">
                     <div className={`h-screen w-screen ${'card' + count} flex flex-col items-center justify-center transition-all`}>
-                        <h1 className="my-5 mr-40 select-none text-start text-2xl font-light text-stone-300">
-            Venha conquistar {titles[count].endsWith('a') ? 'sua' : 'seu'}
+                        <h1 className="my-5 mr-40 select-none text-start text-2xl font-light text-stone-300 rounded-lg px-2 bg-black/30">
+                            Venha conquistar {titles[count].endsWith('a') ? 'sua' : 'seu'}
                         </h1>
                         <TextLink title={titles[count]} link={links[count]} />
-                        <h1 className="my-5 ml-40 select-none text-end text-2xl font-light text-stone-300">
-            com a gente!
+                        <h1 className="my-5 ml-40 select-none text-end text-2xl font-light text-stone-300 rounded-lg px-2 bg-black/30">
+                            com a gente!
                         </h1>
                     </div>
                     <div className="text-cente absolute right-0 z-20 px-10">
@@ -57,38 +57,3 @@ export default function CarouselBanner() {
         </div>
     );
 }
-
-{/* <Image 
-src={images[count]} 
-alt={titles[count]}
-layout="fill"
-objectFit="cover"/> */}
-
-{/* <div
-id="Home"
-className={`h-screen w-screen ${'card' + count} transition-all`}
->
-<Glass>
-    <CardBanner size="w-3/5 h-2/5">
-        <div
-            className={`h-screen w-screen ${'card' + count} flex flex-col items-center justify-center transition-all`}
-        >
-            <h1 className="my-5 mr-40 select-none text-start text-2xl font-light text-stone-300">
-  Venha conquistar {titles[count].endsWith('a') ? 'sua' : 'seu'}
-            </h1>
-            <TextLink title={titles[count]} link={links[count]} />
-            <h1 className="my-5 ml-40 select-none text-end text-2xl font-light text-stone-300">
-  com a gente!
-            </h1>
-        </div>
-        <div className="text-cente absolute right-0 z-20 px-10">
-            <ArrowButton size="text-5xl" onClick={increment} />
-        </div>
-        <div className="text-cente absolute left-0 z-20 -scale-x-100 px-10">
-            <ArrowButton size="text-5xl" onClick={decrement} />
-        </div>
-        <div className="absolute left-0 z-10 h-screen w-28 bg-gradient-to-r from-black/70 px-10"></div>
-        <div className="absolute right-0 z-10 h-screen w-28 bg-gradient-to-l from-black/70 px-10"></div>
-    </CardBanner>
-</Glass>
-</div> */}
