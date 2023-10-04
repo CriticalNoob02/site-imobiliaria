be2eeb0ce85b01da9657ae70ed657a0793d2cb10
AdrielDevelop <adrieldchavea20@gmail.com>
2023-10-03 23:05:42 -0300

    feat: add stats in total properties

--- src/app/components/molecules/filterCards/FilterCards.tsx
+++ src/app/components/molecules/filterCards/FilterCards.tsx
@@ -9,18 +9,31 @@ export default function FilterCards ({
     imageTopics
 }:CardsHoverProps) {
 
+    const total = images.length
+    const case1 = `Apenas ${total} imóvel foi encontrado para você ;)`
+    const case2 = `${total} imóveis encontrados para você ;)`
+    const case0 = 'Nenhum imóvel foi encontrado :('
+    const cases = total > 1 ? case2 : case1
+
     return(
-        <div className="h-full w-full justify-center flex flex-row flex-wrap overflow-hidden">
-            { images.map((image,i) => (
-            <div key={'imovel'+i} className=" w-96 h-96 m-5 mb-20">
-                <CardHover image={image} imageCod={imagesCod[i]} imageTitle={imageTitles[i]} imageValue={imageValues[i]} imageTopic={{
-                    bedrooms: imageTopics[i].bedrooms,
-                    groundSize: imageTopics[i].groundSize,
-                    garages: imageTopics[i].garages,
-                    showers: imageTopics[i].showers
-                }}/>
+        <div className="h-full w-full">
+            <div className="mt-8 px-8 ">
+                <p className="border-b-2 border-x-stone-600 text-stone-600">
+                    {total ? cases : case0}
+                </p>
             </div>
-            ))}
+            <div className="justify-center flex flex-row flex-wrap overflow-hidden">
+                {images.map((image,i) => (
+                    <div key={'imovel'+i} className=" w-96 h-96 m-5 mb-20">
+                        <CardHover image={image} imageCod={imagesCod[i]} imageTitle={imageTitles[i]} imageValue={imageValues[i]} imageTopic={{
+                            bedrooms: imageTopics[i].bedrooms,
+                            groundSize: imageTopics[i].groundSize,
+                            garages: imageTopics[i].garages,
+                            showers: imageTopics[i].showers
+                        }}/>
+                    </div>
+                ))}
+            </div>
         </div>
     )
 }
