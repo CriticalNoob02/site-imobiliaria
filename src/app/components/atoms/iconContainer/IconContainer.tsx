a852c61c81b0dffe2ede1acdf4482566a6f47869
AdrielDevelop <adrieldchavea20@gmail.com>
2023-10-15 21:20:51 -0300

    refactor: change text color

--- src/app/components/atoms/iconContainer/IconContainer.tsx
+++ src/app/components/atoms/iconContainer/IconContainer.tsx
@@ -2,7 +2,7 @@ import IconContainerProps from './types';
 
 export default function IconContainer({ children, size, hover }: IconContainerProps) {
     return(
-        <div className={`grow ${size ?? 'text-[10rem]'} ${hover ?? ''} text-slate-50 transition flex justify-center items-center`}>
+        <div className={`grow ${size ?? 'text-[10rem]'} ${hover ?? ''} text-slate-100 transition flex justify-center items-center`}>
             {children}
         </div>
     )
